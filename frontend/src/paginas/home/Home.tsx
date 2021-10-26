dced16d5b29fcef8abd8f415fe361e8f99feb2b6
caiquebatistam <88943229+caiquebatistam@users.noreply.github.com>
2021-10-25 23:37:08 -0300

    Criando ambiente blog-pessoal no React

---
+++ frontend/src/paginas/home/Home.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./Home.css";
+
+function Home(){
+    return(
+    <>
+        <h1 className="titulo"> Home </h1>
+        <img src="https://upload.wikimedia.org/wikipedia/commons/d/d1/Batman.png" alt="Imagem tela inicial" className="img"/>
+    </>
+   ); 
+}
+export default Home;
